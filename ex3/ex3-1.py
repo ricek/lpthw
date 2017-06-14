fa3bc5efd3d5569a9707002ab144510660a71121
Liang Gao <lianggao@outlook.com>
2017-06-13 20:13:07 -0400

    Lesson 3 and exercise

---
+++ ex3/ex3-1.py
@@ -0,0 +1,35 @@
+# This prints out a line
+print("I will now count my chickens:")
+
+# Calculate number of hens using slash then plus
+print("Hens", 25 + 30 / 6)
+# Calculate number of roosters using asterisk, modulus then minus
+print("Roosters", 100 - 25 * 3 % 4)
+
+# This prints out another line
+print("Now I will count the eggs:")
+
+# Calculate percent, slash the rest from left to right
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# This prints out yet another line
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Calculate first then compare
+print(3 + 2 < 5 - 7)
+
+# Prints the string follow by calculation of plus
+print("What is 3 + 2?", 3 + 2)
+# Prints the string follow by calculation of minus
+print("What is 5 - 7?", 5 - 7)
+
+# This prints out one more line
+print("Oh, that's why it's False.")
+
+# This prints out one more line
+print("How about some more.")
+
+# Prints the string first then compare the two values and print the answer
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
