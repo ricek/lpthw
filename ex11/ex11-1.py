948afec04be0249c1ada09d70928e2ac5df1cb11
Liang Gao <lianggao@outlook.com>
2017-06-16 22:48:19 -0400

    Lesson 11, 12 and execrises

---
+++ ex11/ex11-1.py
@@ -0,0 +1,9 @@
+# Prompt the user to enter their name
+# end='' arg tells print function to not end the line
+print("First Name:", end=' ')
+first = input()
+print("Last Name:", end=' ')
+last = input()
+
+# Prints out formatted string with the given input
+print(f"Hello {first} {last}")
